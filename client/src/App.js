95de38065ee7b6dd986221dc4135feb9284343fb
Fernando Lamas <lamas.fernando1419@gmail.com>
2020-01-31 19:18:47 -0300

    Added BookList component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import BookList from './components/BookList';
 
 function App() {
   return (
     <div className="App">
-        <h1>Testing FrontEnd App</h1>  
+        <h1>Testing FrontEnd App</h1>
+        <BookList />
     </div>
   );
 }
