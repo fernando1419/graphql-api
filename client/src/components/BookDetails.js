8291759143f15625837cf44ac9cd6ff4d31d29bc
Fernando Lamas <lamas.fernando1419@gmail.com>
2020-02-03 22:26:41 -0300

    Finished BookDetails component implementation

--- client/src/components/BookDetails.js
+++ client/src/components/BookDetails.js
@@ -3,13 +3,46 @@ import { graphql } from 'react-apollo';
 import { getBookQuery } from '../queries/queries';
 
 class BookDetails extends Component {
+
+    displayBookDetails() {
+        // this.props.data; // son los datos devueltos de la consulta.
+        const { book } = this.props.data // ES6 sintaxis, es igual que hacer:
+        // const book = this.props.data.book
+        if (book) {
+            return (
+                <div>
+                    <h2> { book.name } </h2>
+                    <p> { book.genre } </p>
+                    <p> { book.author.name } </p>
+                    <p> All books by this author: </p>
+                    <ul className="other-books">
+                    {
+                        book.author.books.map(book => <li key={ book.id } > { book.name } </li> )                        
+                    }
+                    </ul>
+                </div>
+            )
+        }
+
+        return (<div> No book selected... </div>)
+    }
+
     render() {
+        console.log(this.props)
         return (
             <div id="book-details">
-                <p> Output book details here </p>
+                { this.displayBookDetails() }
             </div>
         );
     }
 }
 
-export default graphql(getBookQuery)(BookDetails);
+export default graphql(getBookQuery, {
+    options: function(props) { // cada vez que se actualizan las props, se dispara esta función, que se pasará a la query.
+        return {
+            variables: { // variables para la consulta
+                id: props.bookIdValue
+            }
+        }
+    }
+})(BookDetails);
--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -3,29 +3,42 @@ import { graphql } from 'react-apollo';
 import { getBooksQuery } from '../queries/queries';
 import BookDetails from './BookDetails';
 
-function displayBooks(props) {
-    var data = props.data;
-    // console.log(data);
-    
-    if (data.loading) {
-        return ( <div>Loading books...</div> );
-    } else {
-        return data.books.map(function(book){
-            return (<li key={book.id} > { book.name } </li>);
-        });
+class BookList extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            selected: null
+        }
     }
     
-}
-
-function BookList(props) {
-    return ( 
-        <div>
-            <ul id="book-list">
-                { displayBooks(props) }
-            </ul>
-            <BookDetails />
-        </div>
-    );
+    displayBooks() {
+        var data = this.props.data;
+        // console.log(data);
+        // console.log(this.state.selected)
+    
+        if (data.loading) {
+            return ( <div>Loading books...</div> );
+        } 
+        
+        return data.books.map((book) => {
+            return (
+                <li key={book.id} onClick={ (e) => this.setState({ selected: book.id }) } > { book.name } </li>
+            );
+        });        
+    }
+   
+    render() {
+        // console.log(this.state.selected)
+        return ( 
+            <div>
+                <ul id="book-list">
+                    { this.displayBooks() }
+                </ul>
+                <BookDetails bookIdValue={ this.state.selected } />                
+            </div>
+        );
+    }
 }
 
 export default graphql(getBooksQuery) (BookList);
